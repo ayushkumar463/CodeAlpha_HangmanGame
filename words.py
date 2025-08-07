d598707b6a925cd3c0f8414d2896161a4ea3e01d
ayushkumar463 <ayushkumar754197087@gmail.com>
2025-08-07 19:32:49 +0530

    Add files via upload

---
+++ words.py
@@ -0,0 +1,23 @@
+lst=["ashish","ayush","amit","virat","sanket","ritesh"] 
+
+fruits = [
+    "apple", "banana", "orange", "mango", "grapes", "pear", "peach", "kiwi", "papaya", "plum",
+    "apricot", "cherry", "pineapple", "watermelon", "blueberry", "strawberry", "guava", "fig", "lychee"
+]
+
+technology = [
+    "computer", "keyboard", "monitor", "laptop", "internet", "software", "hardware",
+    "programming", "function", "variable", "python", "java", "binary", "network", "firewall",
+    "algorithm", "database", "cloud", "debug", "compile"
+]
+
+places = [
+    "school", "college", "library", "airport", "hospital", "museum", "temple", "stadium",
+    "village", "city", "desert", "mountain", "island", "forest", "ocean", "beach", "cave", "market"
+]
+
+general = [
+    "secret", "mystery", "puzzle", "treasure", "adventure", "journey", "danger", "mission", "friend",
+    "family", "holiday", "laugh", "magic", "zebra", "elephant", "giraffe", "sunshine", "rainbow",
+    "thunder", "storm", "breeze", "mirror", "button", "shadow", "whisper", "garden", "bottle", "candle"
+]
